916c74913935f93f6cf0c17706b8202d342059a7
Konstantin Mamonov <stanze.von.saidenberg@yandex.ru>
2022-01-19 12:53:45 +0300

    Added file initialState, where put state of app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,41 +6,9 @@ import reducer from '../../reducer/reducer';
 import AddModal from '../AddModal/AddModal';
 import EditModal from '../EditModal/EditModal';
 import DeleteModal from '../DeleteModal/DeleteModal';
+import initialState from '../../files/initialState';
 
 function App() {
-
-  const initialState = {
-    users: [
-      {
-        id: 1, surname: 'Иванов', name: 'Иван', patronymic: 'Иванович', email: 'mail1@mail.com', login: 'user1',
-      },
-      {
-        id: 2, surname: 'Петров', name: 'Петр', patronymic: 'Сергеевич', email: 'mail2@mail.com', login: 'user2',
-      },
-      {
-        id: 3, surname: 'Сергеев', name: 'Григорий', patronymic: 'Викторович', email: 'mail3@mail.com', login: 'user3',
-      },
-      {
-        id: 4, surname: 'Федоров', name: 'Виктор', patronymic: 'Федорович', email: 'mail4@mail.com', login: 'user4',
-      },
-      {
-        id: 5, surname: 'Хвастунов', name: 'Сергей', patronymic: 'Петрович', email: 'mail5@mail.com', login: 'user5',
-      },
-      {
-        id: 6, surname: 'Григорьев', name: 'Федор', patronymic: 'Григорьевич', email: 'mail6@mail.com', login: 'user6',
-      },
-    ],
-    modals: [
-      {
-        modalEditActive: false, user: { id: -1, surname: '', name: '', patronymic: '', email: '', login: ''}
-      },
-      {
-        modalDeleteActive: false, id: -1
-      }
-    ]
-      
-    
-  };
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
